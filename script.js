68f84ae2cb521db8034ba1b22b462d92f7888676
Vladimir Patrikeyev <patrik666@gmail.com>
2022-09-24 18:20:04 +0300

    first

---
+++ script.js
@@ -0,0 +1,69 @@
+import modulesConst from './modules/constans.js';
+import modulesTable from './modules/table.js';
+import modulesForm from './modules/form.js';
+
+const {
+  btnAdd,
+  overlayModal,
+  btnClose,
+  modalForm,
+  tBody,
+  tHead,
+  arrayTr,
+  formModal,
+  checkModal,
+  inputModalSale,
+  modalID,
+  totalPriceModal,
+  inputModalPrice,
+  inputModalCount,
+  totalPriceHeader,
+  totalPriceArray,
+  // ID,
+  // totalPriceALL        
+} = modulesConst;
+
+const {
+  createRow,
+  calculationTotalPriceTable,
+  deleteTr,
+  openIMG,
+  openModal,
+  closeModal 
+} = modulesTable;
+
+const {
+  checkboxInput,
+    sendingForm,
+    calculationTotalPrice,
+    priceFocus,
+    countFocus,
+    saleFocus,
+} = modulesForm;
+
+
+
+
+
+//функция запуска всех функций INIT
+{
+const init = () => {
+
+  calculationTotalPriceTable();
+  openModal();
+  closeModal();
+  deleteTr();
+  openIMG();
+  checkboxInput();
+  sendingForm();
+  priceFocus();
+  countFocus();
+  saleFocus();
+
+}
+
+window.CRMinit = init;
+}
+
+
+
